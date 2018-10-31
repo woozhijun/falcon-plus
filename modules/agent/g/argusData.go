ec5906284cca31bf355caeb5fce610e021d17d24
wuzhijun <wuzhijun@mobike.com>
2018-10-31 19:31:49 +0800

    fix, parser argus metric

--- modules/agent/g/argusData.go
+++ modules/agent/g/argusData.go
@@ -4,12 +4,13 @@ import (
 	"log"
 	"github.com/open-falcon/falcon-plus/common/model"
 
-	"math/rand"
-	"time"
 	"encoding/json"
+	"reflect"
+	"strconv"
+	"errors"
 )
 
-func reportArgusMetrics(metrics []*model.MetricValue, resp *model.TransferResponse)  {
+func reportArgusMetrics(metrics []*model.MetricValue, resp *model.TransferResponse) {
 
 	result, err := json.Marshal(metrics)
 	if err != nil {
@@ -17,39 +18,63 @@ func reportArgusMetrics(metrics []*model.MetricValue, resp *model.TransferRespon
 	}
 	log.Println("result: " + string(result))
 
-	result1, err := json.Marshal(parseArgusMetric(metrics))
-	if err != nil {
-		log.Println("argus json paser failed. ", err)
+	for endpoint, data := range createArgusMetric(metrics) {
+		tmp,_ := json.Marshal(data)
+		log.Println("endpoint: " + endpoint)
+		log.Println("argus Metric: " + string(tmp))
 	}
-	log.Println("argus Metric Result:" + string(result1))
-	rand.Seed(time.Now().UnixNano())
 }
 
-func parseArgusMetric(metrics []*model.MetricValue) (*model.ArgusMetric) {
+func parseFloat64(value interface{}) (float64, error) {
+	tmp := reflect.ValueOf(value)
+	switch value.(type) {
+	case string:
+		return strconv.ParseFloat(tmp.String(), 64)
+	case int64, int32, int16, int8, int:
+		return float64(tmp.Int()), nil
+	case uint64, uint32, uint16, uint8, uint:
+		return float64(tmp.Uint()), nil
+	case float64, float32:
+		return tmp.Float(), nil
+	default:
+		return float64(0), errors.New("not match value for type -> " + tmp.Type().String())
+	}
+}
+
+func createArgusMetric(metrics []*model.MetricValue) (map[string]*model.ArgusMetric) {
 	if metrics == nil {
-		return nil
+		return make(map[string]*model.ArgusMetric)
 	}
 
-	argusMetrics := make(map[string]float64)
-	tags := make(map[string]string)
+	resultData := make(map[string]*model.ArgusMetric)
 	for _, metric := range metrics {
-		value,ok := metric.Value.(float64)
-		if !ok {
-			log.Println("metric value parse error. Value:" + metric.Value.(string))
+
+		data, err := parseFloat64(metric.Value)
+		if err != nil {
+			log.Println("metricValue parseFloat error." + err.Error())
 			continue
 		}
-		argusMetrics[metric.Metric] = value
-		tags["endpoint"] = metric.Endpoint
-	}
+		metricsResult := resultData[metric.Endpoint]
+		if metricsResult == nil {
+			argusMetric := &model.ArgusMetric{}
+			argusMetric.Group = "open-falcon"
+			argusMetric.Service = "falcon-agent"
+			argusMetric.MetricType = "falcon"
+			argusMetric.Step = metric.Step
+			argusMetric.MeterType = metric.Metric
+			argusMetric.Timestamp = metric.Timestamp * 1000
 
-	return &model.ArgusMetric{
-		Group:"open-falcon",
-		Service:"falcon-agent",
-		Metrics:argusMetrics,
-		Tags:tags,
-		MetricType:"falcon",
-		MeterType:"",
-		Step:60,
-		Timestamp:time.Now().UnixNano(),
+			tags := make(map[string]string)
+			tags["endpoint"] = metric.Endpoint
+			argusMetric.Tags = tags
+
+			initMetrics := make(map[string]float64)
+			initMetrics[metric.Metric] = data
+			argusMetric.Metrics = initMetrics
+			resultData[metric.Endpoint] = argusMetric
+		} else {
+			metricsResult.Metrics[metric.Metric] = data
+		}
 	}
+	return resultData
 }
