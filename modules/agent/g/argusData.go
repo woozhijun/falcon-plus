4d49d3a8885da648d99696a63fc80b17e70ac0c4
wuzhijun <wuzhijun@mobike.com>
2018-11-02 18:09:10 +0800

    parser tags

--- modules/agent/g/argusData.go
+++ modules/agent/g/argusData.go
@@ -8,6 +8,7 @@ import (
 	"reflect"
 	"strconv"
 	"errors"
+	"strings"
 )
 
 func reportArgusMetrics(metrics []*model.MetricValue, resp *model.TransferResponse) {
@@ -18,9 +19,9 @@ func reportArgusMetrics(metrics []*model.MetricValue, resp *model.TransferRespon
 	}
 	log.Println("result: " + string(result))
 
-	for endpoint, data := range createArgusMetric(metrics) {
+	for endpointKey, data := range createArgusMetric(metrics) {
 		tmp,_ := json.Marshal(data)
-		log.Println("endpoint: " + endpoint)
+		log.Println("endpoint: " + endpointKey)
 		log.Println("argus Metric: " + string(tmp))
 	}
 }
@@ -41,6 +42,19 @@ func parseFloat64(value interface{}) (float64, error) {
 	}
 }
 
+func parseTags(tagStr string) (map[string]string) {
+	if tagStr == "" {
+		return make(map[string]string)
+	}
+	tags := make(map[string]string)
+	tvs := strings.Split(tagStr, ",")
+	for _, tks := range tvs {
+		tk := strings.Split(tks, "=")
+		tags[tk[0]] = tk[1]
+	}
+	return tags
+}
+
 func createArgusMetric(metrics []*model.MetricValue) (map[string]*model.ArgusMetric) {
 	if metrics == nil {
 		return make(map[string]*model.ArgusMetric)
@@ -54,7 +68,8 @@ func createArgusMetric(metrics []*model.MetricValue) (map[string]*model.ArgusMet
 			log.Println("metricValue parseFloat error." + err.Error())
 			continue
 		}
-		metricsResult := resultData[metric.Endpoint]
+		endpointKey := metric.Endpoint + "&"+ metric.Tags
+		metricsResult := resultData[endpointKey]
 		if metricsResult == nil {
 			argusMetric := &model.ArgusMetric{}
 			argusMetric.Group = "open-falcon"
@@ -64,14 +79,14 @@ func createArgusMetric(metrics []*model.MetricValue) (map[string]*model.ArgusMet
 			argusMetric.MeterType = metric.Metric
 			argusMetric.Timestamp = metric.Timestamp * 1000
 
-			tags := make(map[string]string)
+			tags := parseTags(metric.Tags)
 			tags["endpoint"] = metric.Endpoint
 			argusMetric.Tags = tags
 
 			initMetrics := make(map[string]float64)
 			initMetrics[metric.Metric] = data
 			argusMetric.Metrics = initMetrics
-			resultData[metric.Endpoint] = argusMetric
+			resultData[endpointKey] = argusMetric
 		} else {
 			metricsResult.Metrics[metric.Metric] = data
 		}
